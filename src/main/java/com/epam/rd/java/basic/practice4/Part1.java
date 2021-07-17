1448b99f9adb8698f84a532374a59b1253f1959f
Liubomyr Antonyk <liubchyk.antonyk@gmail.com>
2021-07-17 11:37:54 +0300

    Task 1.2

--- src/main/java/com/epam/rd/java/basic/practice4/Part1.java
+++ src/main/java/com/epam/rd/java/basic/practice4/Part1.java
@@ -1,12 +1,17 @@
 package com.epam.rd.java.basic.practice4;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Part1 {
 
     public static void main(String[] args) {
-        System.out.println(Util.getInput("file1.txt"));
+        System.out.println(getInput("file1.txt"));
     }
 
     public  static String dellTwoChars(String input){
@@ -19,6 +24,21 @@ public class Part1 {
             else{
                 sb.append(m.group(1));
             }
+        }
+        return sb.toString();
+    }
+    public static String getInput(String fileName) {
+        Logger logger = Logger.getAnonymousLogger();
+        StringBuilder sb = new StringBuilder();
+        try {
+            Scanner scanner = new Scanner(new File(fileName), "Cp1251");
+            while (scanner.hasNextLine()) {
+                sb.append(scanner.nextLine()).append(System.lineSeparator());
+            }
+            scanner.close();
+            return sb.toString().trim();
+        } catch (IOException ex) {
+            logger.log(Level.SEVERE, "IOException");
         }
         return sb.toString();
     }
